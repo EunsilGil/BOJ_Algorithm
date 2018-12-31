60134dcfc5482e9dd2d378c210fdb5f3a58e28f1
Dream <44895889+EunsilGil@users.noreply.github.com>
2018-12-31 14:13:25 +0900

    Create BOJ_2293_Coin.cpp

---
+++ DP/BOJ_2293_Coin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+  int n, k;
+  int coin[101];
+  int dp[10001] = {0};
+  cin >> n >> k;
+
+  for(int i = 0; i < n; i++)
+    cin >> coin[i];
+
+  dp[0] = 1;
+  for(int i = 0; i < n; i++){
+    for(int j = coin[i]; j <= k; j++){
+        dp[j] += dp[j-coin[i]];
+    }
+  }
+  cout << dp[k];
+  return 0;
+}
